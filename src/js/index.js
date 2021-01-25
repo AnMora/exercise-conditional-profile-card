09b484769cacb2b699b838eb3a42b60b99d17157
Anmora96 <anmora.2396@gmail.com>
2021-01-25 23:57:43 +0000

    Se entiende el ejercicio

--- src/js/index.js
+++ src/js/index.js
@@ -30,12 +30,13 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   let name = variables.name;
+  let lastname = variables.lastname;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${name}</h1>
+          <h1>${name} ${lastname}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
